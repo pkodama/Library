4b278591220718c398bc964fad98cdd82e45ea7c
pkodama <49176411+pkodama@users.noreply.github.com>
2020-05-21 13:39:38 +0900

    splay

--- splay-tree.cpp
+++ splay-tree.cpp
@@ -23,6 +23,90 @@ template <typename T1, typename T2>ostream &operator<<(ostream &out, const map<T
 template <typename T1, typename T2>ostream &operator<<(ostream &out, const pair<T1, T2> &p){out << "(" << p.first << ", " << p.second << ")";return out;}
 template<class T>void debag(const T &obj){cout<<obj<<endl;}
 
+struct SplayTree{
+    struct SplayNode{
+        SplayNode *right,*left,*parent;
+        int size;
+        int value;
+
+        SplayNode(){
+            left=nullptr;
+            right=nullptr;
+            parent=nullptr;
+            size=1;
+        }
+
+        void rotate(){
+            SplayNode *c,*p,*pp;
+            p=this->parent;
+            pp=p->parent;
+
+            if(p->left==this){
+                c=this->right;
+                this->right=p;
+                p->left=c; 
+            }else{
+                c=this->left;
+                this->left=p;
+                p->right=c; 
+            }
+
+            if(pp && pp->left==p)pp->left=this;
+            if(pp && pp->right==p)pp->right=this;
+            this->parent=pp;
+            p->parent=this;
+            if(c)c->parent=p;
+            
+            p->update();
+            this->update();
+        }
+
+        int state(){
+            if(!parent)return 0;
+            if(parent->left==this)return 1;
+            if(parent->right==this)return -1;
+            return 0;
+        }
+        
+        void splay(){
+            while(parent){
+                if(parent->state()==0){
+                    this->rotate();
+                }else if(this->state()==parent->state()){
+                    this->parent->state();
+                    this->state();
+                }else{
+                    this->state();
+                    this->state();
+                }
+            }
+        }
+
+        void update(){
+            size=1;
+            if(this->left)this->size+=this->left->size;
+            if(this->right)this->size+=this->right->size;
+        }
+    };
+
+
+    SplayNode *get(int ind,SplayNode *root){
+        SplayNode *now=root;
+        while(1){
+            int lsize=(now->left ? now->left->size : 0);
+            if(ind < lsize){
+                now=now->left;
+            }else if(ind==lsize){
+                now->splay();
+                return now;
+            }else if(ind > lsize){
+                now=now->right;
+                ind=ind-lsize-1;
+            }
+        }
+    }
+};
+
 bool solve(){
     return false;
 }
